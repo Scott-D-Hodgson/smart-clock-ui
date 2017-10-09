ea78f8e115a3add1b56fab88e97ddd7407181f76
Scott Hodgson <Scott-D-Hodgson@users.noreply.github.com>
2017-10-09 14:07:02 -0400

    Updated the time component display (#16)
    Added to master.

--- src/app/time/time.component.ts
+++ src/app/time/time.component.ts
@@ -11,24 +11,18 @@ export class TimeComponent implements OnInit {
 
   private hour: string; 
   private minute: string;
-  private second: string;
+  private ampm: string;
+  //private second: string;
   private subscription: Subscription;
 
   constructor() { 
-    let date = new Date();
-    this.hour = date.getHours().toString();
-    this.minute = ('0' + date.getMinutes()).slice(-2);
-    this.second = ('0' + date.getSeconds()).slice(-2);    
+    this.timerUpdate(); 
   }
 
   ngOnInit() {
-    let timer = TimerObservable.create(2000, 1000);
+    let timer = TimerObservable.create(60000, 60000);
     this.subscription = timer.subscribe(t => {
-      let date = new Date();
-      this.hour = date.getHours().toString();
-      this.minute = ('0' + date.getMinutes()).slice(-2);
-      this.second = ('0' + date.getSeconds()).slice(-2);
-      //this.currentDate = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
+      this.timerUpdate();
     })
   }
 
@@ -36,4 +30,13 @@ export class TimeComponent implements OnInit {
     this.subscription.unsubscribe();
   }
 
+  private timerUpdate() {
+    let date = new Date();
+    let hour = date.getHours();
+    if (hour >= 12) { this.ampm = "PM" } else { this.ampm = "AM" };
+    if (hour == 0) { hour = 12 } else if (hour > 12) { hour = hour - 12 };
+    this.hour = date.getHours().toString();
+    this.minute = ('0' + date.getMinutes()).slice(-2);
+    //this.second = ('0' + date.getSeconds()).slice(-2);
+  }
 }
